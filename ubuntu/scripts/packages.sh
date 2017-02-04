6a70d2383eacd04143a0cfa34479c7f236118f65
Wyatt Anderson <wanderson@gmail.com>
2017-02-04 23:07:44 +0000

    install salt

--- ubuntu/scripts/packages.sh
+++ ubuntu/scripts/packages.sh
@@ -20,3 +20,8 @@ if [[ $INSTALL_DEV_PACKAGES  =~ true || $INSTALL_DEV_PACKAGES =~ 1 ||
 fi
 
 apt-get -y install $ESSENTIAL_PACKAGES
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list
+apt-get update
+apt-get -y install salt-minion
